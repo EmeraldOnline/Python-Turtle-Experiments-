33eb46c4f18cf6e3ee3506692074cdbf5967cd24
EmeraldOnline <34595686+EmeraldOnline@users.noreply.github.com>
2020-01-09 13:29:56 +0000

    Simplified and Renamed

--- Mesmerising Shapes.py
+++ Mesmerising Shapes.py
@@ -1,15 +1,11 @@
 import turtle
-import random
-import time
 
-t = turtle.Pen()
-t.speed(1000)
+turtle.speed(1000)
 turtle.tracer(0)
 
-for i in range(0,72000):
+for i in range(72000):
     angle = i/100
     repeat = int(angle*1.5)
-    turtle.clear()
     for i in range(0,repeat):
         turtle.forward(i*2)
         turtle.right(angle)
